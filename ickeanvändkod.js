43422af4e68fab6993fe402bce2358156edef72a
root <root@Jonah.localdomain>
2021-01-18 16:26:01 +0100

    decrypt fungerar bra

---
+++ ickeanvändkod.js
@@ -0,0 +1,28 @@
+// console.log(charkoder)
+
+// let charkod = secretmess[i].charCodeAt(0)
+// res = res + String.fromCharCode()
+// console.log(secretmess[i],charkod, charkod + 3, String.fromCharCode(charkod + 3))
+
+// const krypterm = []
+// function enkryptm(mess, steg) {
+//     for (let i = 0; i < krypterm.length; i++) {
+//         krypterm.push(mess[i].charCodeAt(0))
+//     }
+    
+//     for (let i = 0; i < krypterm.length; i++) {
+//         krypterm[i] = krypterm[i] + 1
+//     }
+    
+//     let result = ""
+//     for (let i = 0; i < krypterm.length; i++) {
+//         result = result + String.fromCharCode(krypterm[i])
+//     }
+
+// return result
+// }
+
+// enkryptm("hejpådig", 5);
+
+
+
--- majn.js
+++ majn.js
@@ -5,23 +5,18 @@ console.log(secretmess)
 const charkoder = []
 
 let krypter = ""
+let kryptering = ""
 
-//Städa utan att ändra (ATT GÖRA)
+//Städa
 
 function enkrypt() {
     for (let i = 0; i < secretmess.length; i++) {
         charkoder.push(secretmess[i].charCodeAt(0))
-    
-        // let charkod = secretmess[i].charCodeAt(0)
-        // res = res + String.fromCharCode()
-        // console.log(secretmess[i],charkod, charkod + 3, String.fromCharCode(charkod + 3))
     }
     
     for (let i = 0; i < charkoder.length; i++) {
-        charkoder[i] = charkoder[i] + 1
+        charkoder[i] = charkoder[i] + 2
     }
-    
-    // console.log(charkoder)
     
     for (let i = 0; i < charkoder.length; i++) {
         krypter = krypter + String.fromCharCode(charkoder[i])
@@ -31,18 +26,17 @@ console.log(krypter)
 
 enkrypt();
 
-
 function dekrypt() {
+    console.log(krypter)
     for (let i = 0; i < krypter.length; i++) {
         charkoder.push(krypter[i].charCodeAt(0))
     }
 
     for (let i = 0; i < charkoder.length; i++) {
-        charkoder[i] = charkoder[i] - 1
+        charkoder[i] = charkoder[i] - 2
     }
 
-    let kryptering = ""
-    for (let i = 0; i < charkoder.length; i++) {
+    for (let i = 0; i < krypter.length; i++) {
         kryptering = kryptering + String.fromCharCode(charkoder[i])
     }
 console.log(kryptering)
@@ -50,27 +44,4 @@ console.log(kryptering)
 
 dekrypt();
 
-
-
-// const krypterm = []
-// function enkryptm(mess) {
-//     for (let i = 0; i < krypterm.length; i++) {
-//         krypterm.push(mess[i].charCodeAt(0))
-//     }
-    
-//     for (let i = 0; i < krypterm.length; i++) {
-//         krypterm[i] = krypterm[i] + 1
-//     }
-    
-//     let result = ""
-//     for (let i = 0; i < krypterm.length; i++) {
-//         result = result + String.fromCharCode(krypterm[i])
-//     }
-
-// return result
-// }
-
-// enkryptm();
-
-
-
+const svarbakom = "BE NICE TO EACH OTHER AND THE PLANET PLEASE"
