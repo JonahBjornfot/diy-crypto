283fb80be63f955b36cece0f02c3a07f215ff90d
root <root@Jonah.localdomain>
2021-01-18 14:13:01 +0100

    städat koden

--- majn.js
+++ majn.js
@@ -1,20 +1,33 @@
 const secretmess = "JULKISPULKIS"
 
+console.log(secretmess)
+
 const charkoder = []
 
-// console.log(secretmess.charCodeAt(0))
+//Städa utan att ändra (ATT GÖRA)
 
-let res = ""
-
-for (let i = 0; i < secretmess.length; i++) {
-    charkoder.push(secretmess[i].charCodeAt(0))
-
-    // let charkod = secretmess[i].charCodeAt(0)
-    // res = res + String.fromCharCode()
-    // console.log(secretmess[i],charkod, charkod + 3, String.fromCharCode(charkod + 3))
+function enkrypt(mess) {
+    for (let i = 0; i < secretmess.length; i++) {
+        charkoder.push(secretmess[i].charCodeAt(0))
+    
+        // let charkod = secretmess[i].charCodeAt(0)
+        // res = res + String.fromCharCode()
+        // console.log(secretmess[i],charkod, charkod + 3, String.fromCharCode(charkod + 3))
+    }
+    
+    for (let i = 0; i < charkoder.length; i++) {
+        charkoder[i] = charkoder[i] + 1
+    }
+    
+    // console.log(charkoder)
+    
+    let krypter = ""
+    for (let i = 0; i < charkoder.length; i++) {
+        krypter = krypter + String.fromCharCode(charkoder[i])
+    }
+console.log(krypter)
 }
 
-console.log(charkoder)
-
+enkrypt();
 
 
