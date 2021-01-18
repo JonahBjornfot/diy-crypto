941aed672487aea1d0fc89c6c04e4c55ae7d1c39
root <root@Jonah.localdomain>
2021-01-18 14:21:39 +0100

    Dubbla funktioner

--- majn.js
+++ majn.js
@@ -6,7 +6,7 @@ const charkoder = []
 
 //Städa utan att ändra (ATT GÖRA)
 
-function enkrypt(mess) {
+function enkrypt() {
     for (let i = 0; i < secretmess.length; i++) {
         charkoder.push(secretmess[i].charCodeAt(0))
     
@@ -29,5 +29,30 @@ console.log(krypter)
 }
 
 enkrypt();
+
+// function enkryptm(mess) {
+//     for (let i = 0; i < mess.length; i++) {
+//         charkoder.push(mess[i].charCodeAt(0))
+    
+//         // let charkod = secretmess[i].charCodeAt(0)
+//         // res = res + String.fromCharCode()
+//         // console.log(secretmess[i],charkod, charkod + 3, String.fromCharCode(charkod + 3))
+//     }
+    
+//     for (let i = 0; i < mess.length; i++) {
+//         charkoder[i] = mess[i] + 1
+//     }
+    
+//     // console.log(charkoder)
+    
+//     let krypter = ""
+//     for (let i = 0; i < mess.length; i++) {
+//         krypter = krypter + String.fromCharCode(mess[i])
+//     }
+// console.log(krypter)
+// }
+
+// enkryptm();
+
 
 
