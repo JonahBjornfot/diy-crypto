d04b5dec10814efce25b765dc20ea708e787719a
root <root@Jonah.localdomain>
2021-01-18 14:02:11 +0100

    Charkoder

--- majn.js
+++ majn.js
@@ -1,8 +1,20 @@
 const secretmess = "JULKISPULKIS"
 
-console.log(secretmess.charCodeAt(0))
+const charkoder = []
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
+// console.log(secretmess.charCodeAt(0))
+
+let res = ""
+
+for (let i = 0; i < secretmess.length; i++) {
+    charkoder.push(secretmess[i].charCodeAt(0))
+
+    // let charkod = secretmess[i].charCodeAt(0)
+    // res = res + String.fromCharCode()
+    // console.log(secretmess[i],charkod, charkod + 3, String.fromCharCode(charkod + 3))
 }
+
+console.log(charkoder)
+
+
+
