e5a8d7987adab03d88d3e5059237f5987c8c47d7
root <root@Jonah.localdomain>
2021-01-18 15:10:58 +0100

    Decrypt fungerar!

--- majn.js
+++ majn.js
@@ -4,6 +4,8 @@ console.log(secretmess)
 
 const charkoder = []
 
+let krypter = ""
+
 //Städa utan att ändra (ATT GÖRA)
 
 function enkrypt() {
@@ -21,7 +23,6 @@ function enkrypt() {
     
     // console.log(charkoder)
     
-    let krypter = ""
     for (let i = 0; i < charkoder.length; i++) {
         krypter = krypter + String.fromCharCode(charkoder[i])
     }
@@ -30,26 +31,43 @@ console.log(krypter)
 
 enkrypt();
 
+
+function dekrypt() {
+    for (let i = 0; i < krypter.length; i++) {
+        charkoder.push(krypter[i].charCodeAt(0))
+    }
+
+    for (let i = 0; i < charkoder.length; i++) {
+        charkoder[i] = charkoder[i] - 1
+    }
+
+    let kryptering = ""
+    for (let i = 0; i < charkoder.length; i++) {
+        kryptering = kryptering + String.fromCharCode(charkoder[i])
+    }
+console.log(kryptering)
+}
+
+dekrypt();
+
+
+
+// const krypterm = []
 // function enkryptm(mess) {
-//     for (let i = 0; i < mess.length; i++) {
-//         charkoder.push(mess[i].charCodeAt(0))
-    
-//         // let charkod = secretmess[i].charCodeAt(0)
-//         // res = res + String.fromCharCode()
-//         // console.log(secretmess[i],charkod, charkod + 3, String.fromCharCode(charkod + 3))
+//     for (let i = 0; i < krypterm.length; i++) {
+//         krypterm.push(mess[i].charCodeAt(0))
 //     }
     
-//     for (let i = 0; i < mess.length; i++) {
-//         charkoder[i] = mess[i] + 1
+//     for (let i = 0; i < krypterm.length; i++) {
+//         krypterm[i] = krypterm[i] + 1
 //     }
     
-//     // console.log(charkoder)
-    
-//     let krypter = ""
-//     for (let i = 0; i < mess.length; i++) {
-//         krypter = krypter + String.fromCharCode(mess[i])
+//     let result = ""
+//     for (let i = 0; i < krypterm.length; i++) {
+//         result = result + String.fromCharCode(krypterm[i])
 //     }
-// console.log(krypter)
+
+// return result
 // }
 
 // enkryptm();
